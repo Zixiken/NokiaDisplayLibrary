57d17769b683b189976af08efc7775b78dd6a428
Michael Zanga <zangamj@appstate.edu>
2017-10-18 10:35:35 -0400

    Implemented initialization, configuration, and send functions

--- include/libnokiadisplay.h
+++ include/libnokiadisplay.h
@@ -1,15 +1,85 @@
+/*
+ * libnokiadisplay - A display library for Nokia 5110/3310 displays. Really
+ * this works with the PCD8544 LCD controller (used by the Nokia displays),
+ * so if other displays exist that use it, this library should still work (I
+ * don't know if they do though). To use this library, the controller first
+ * needs to be initialized as soon as possible. Then, extended register values
+ * should be set. If you don't know, default values should work. Lastly, set
+ * the display mode and power mode to activate the display.
+ *
+ * TODO: finish this
+ */
+
 #ifndef LIBNOKIADISPLAY_H
 #define LIBNOKIADISPLAY_H
 
 #include <stdint.h>
 
-int initPorts(volatile uint8_t * resPort, uint8_t resBit,
+#define DISPLAY_MODE_BLANK 0
+#define DISPLAY_MODE_ALL 1
+#define DISPLAY_MODE_NORMAL 4
+#define DISPLAY_MODE_INVERSE 5
+
+/*
+ * Reset the controller and set the appropriate signals to it. According to the
+ * PCD8544 datasheet, this should be done as soon as possible. Other functions
+ * will not work before this one is called. Parameters are port memory
+ * addresses and respective bits in those addresses, for each pin connected to
+ * the controller (such as address &PORTB, bit  6).
+ * On Adafruit's Nokia breakout board, the corresponding controller pins are:
+ * RST - resPort/Bit
+ * CS - enablePort/Bit
+ * D/C - selectorPort/Bit
+ * DIN - dataPort/Bit
+ * CLK - clockPort/Bit
+ *
+ * Returns false (and no reset action is performed) if any bits are out of
+ * range (over 7), true otherwise.
+ */
+int initController(volatile uint8_t * resPort, uint8_t resBit,
 		volatile uint8_t * enablePort, uint8_t enableBit,
+		volatile uint8_t * selectorPort, uint8_t selectorBit,
 		volatile uint8_t * dataPort, uint8_t dataBit,
 		volatile uint8_t * clockPort, uint8_t clockBit);
-int initData(uint8_t bias, uint8_t vop, uint8_t tc);
+
+/*
+ * Set the bias system, operation voltage, and temperature coefficient
+ * for driving the LCD.
+ *
+ * Returns false if the controller is not initialized
+ * or parameters are out of range, true otherwise.
+ */
+int setExtendedRegisters(uint8_t bias, uint8_t vop, uint8_t tc);
+
+/*
+ * Set the bias system, operation voltage, and temperature coefficient
+ * described above to default values of 4, 0x7f, and 0.
+ *
+ * Returns false if the controller is not initialized, true otherwise.
+ */
+int defaultSetExtendedRegisters();
+
+/*
+ * Set the display mode of the LCD. Possible values are:
+ *   DISPLAY_MODE_BLANK: turn off all display segments
+ *   DISPLAY_MODE_ALL: turn on all display segments
+ *   DISPLAY_MODE_NORMAL: normal display from controller dram
+ *   DISPLAY_MODE_INVERSE: inverse display from controller dram
+ *
+ * Returns false if the controller is not initialized
+ * or an invalid mode is passed, true otherwise.
+ */
 int setDisplayMode(uint8_t mode);
+
+/*
+ * Set the power mode of the LCD. False sets power mode on (which matches the
+ * PCD8544's function set command).
+ *
+ * Returns false if the controller is not initialized, true otherwise.
+ */
 int setPowerMode(uint8_t mode);
-int draw(uint8_t x, uint8_t y, uint8_t state);
-int love(void);
+
+int drawPixel(uint8_t x, uint8_t y, uint8_t state);
+
+void love(void);
 #endif
--- src/libnokiadisplay.c
+++ src/libnokiadisplay.c
@@ -1,51 +1,137 @@
 #include "libnokiadisplay.h"
 #include <util/delay.h>
 
-inline void writeBit(volatile uint8_t * addr, uint8_t bit, uint8_t state) {
-	if(state) *addr |= 1 << bit;
-	else *addr &= ~(1 << bit);
+#define COMMAND_NORMAL 0x20
+#define COMMAND_EXTENDED 0x21
+#define COMMAND_VOP 0x80
+#define COMMAND_TC 0x04
+#define COMMAND_BIAS 0x10
+#define COMMAND_DISPLAY_MODE 0x08
+
+volatile uint8_t * resP, * enableP, * dataP, * clockP, * selP;
+uint8_t resM, enableM, dataM, clockM, selM, initialized = 0, powerMode = 4;
+
+/*
+ * Helper function to turn a bit on or off.
+ */
+inline void writeBit(volatile uint8_t * addr, uint8_t mask, uint8_t state) {
+	if(state) *addr |= mask;
+	else *addr &= ~mask;
 }
 
-int write(uint8_t byte, uint8_t dc) {
-	return 0;
+/*
+ * Send data or a command to the controller. If dc is true, the byte is
+ * interpreted as data, and if false, as a command. The passed byte is sent
+ * MSB-first, with minimal delay between clock edges.
+ *
+ * WARNING: Bit banging as fast as possible works fine on AVR microcontrollers
+ * with 16Mhz or slower clock cycle, but other architectures may not delay
+ * enough between clock edges. The PCD8544 requires a minimum of 100ns pulse
+ * width, minimum of 250ns clock cycle.
+ */
+void send(uint8_t byte, uint8_t dc) {
+	uint8_t mask;
+
+	writeBit(selP, selM, dc);
+
+	for(mask = 0x80; mask; mask >>= 1) {
+		writeBit(dataP, dataM, byte & mask);
+		*clockP |= clockM;
+		*clockP &= ~clockM;
+	}
 }
 
-int initPorts(volatile uint8_t * resPort, uint8_t resBit,
+int initController(volatile uint8_t * resPort, uint8_t resBit,
 		volatile uint8_t * enablePort, uint8_t enableBit,
+		volatile uint8_t * selectorPort, uint8_t selectorBit,
 		volatile uint8_t * dataPort, uint8_t dataBit,
 		volatile uint8_t * clockPort, uint8_t clockBit) {
-	if(resBit < 0 || resBit > 7 ||
-			enableBit < 0 || enableBit > 7 ||
-			dataBit < 0 || dataBit > 7 ||
-			clockBit < 0 || clockBit > 7)
-		return 1;
+	if(resBit > 7 || enableBit > 7 || dataBit > 7 || clockBit > 7 || selectorBit > 7)
+		return 0;
 
-	while(1) {
-		writeBit(resPort, resBit, 0);
-		_delay_ms(500);
-		writeBit(resPort, resBit, 1);
-		_delay_ms(500);
-	}
-	return 0;
+	resM = 1 << resBit;
+	*resPort &= resM;
+
+	resP = resPort;
+	enableP = enablePort;
+	dataP = dataPort;
+	clockP = clockPort;
+	selP = selectorPort;
+
+	enableM = 1 << enableBit;
+	dataM = 1 << dataBit;
+	clockM = 1 << clockBit;
+	selM = 1 << selectorBit;
+
+	*enableP |= enableM;
+	*clockP &= clockM;
+
+	*resP |= resM;
+
+	initialized = 1;
+	return 1;
 }
 
-int initData(uint8_t bias, uint8_t vop, uint8_t tc) {
-	return 0;
+int setExtendedRegisters(uint8_t bias, uint8_t vop, uint8_t tc) {
+	if(!initialized || bias > 7 || vop > 0x7f || tc > 3) return 0;
+
+	*enableP  &= ~enableM;
+	send(COMMAND_EXTENDED | powerMode, 0);
+	send(COMMAND_VOP | vop, 0);
+	send(COMMAND_BIAS | bias, 0);
+	send(COMMAND_TC | tc, 0);
+	send(COMMAND_NORMAL | powerMode, 0);
+	*enableP |= enableM;
+
+	return 1;
+}
+
+int defaultSetExtendedRegisters() {
+	if(!initialized) return 0;
+
+	*enableP  &= ~enableM;
+	send(COMMAND_EXTENDED | powerMode, 0);
+	send(COMMAND_VOP | 0x7f, 0);
+	send(COMMAND_BIAS | 4, 0);
+	send(COMMAND_NORMAL | powerMode, 0);
+	*enableP |= enableM;
+
+	return 1;
 }
 
 int setDisplayMode(uint8_t mode) {
-	return 0;
+	if(!initialized) return 0;
+
+	switch(mode) {
+		case DISPLAY_MODE_BLANK:
+		case DISPLAY_MODE_ALL:
+		case DISPLAY_MODE_NORMAL:
+		case DISPLAY_MODE_INVERSE:
+			*enableP  &= ~enableM;
+			send(COMMAND_DISPLAY_MODE | mode, 0);
+			*enableP |= enableM;
+			return 1;
+		default:
+			return 0;
+	}
 }
 
 int setPowerMode(uint8_t mode) {
+	if(!initialized) return 0;
+
+	if(mode) powerMode = 4;
+	else powerMode = 0;
+	*enableP  &= ~enableM;
+	send(COMMAND_NORMAL | powerMode, 0);
+	*enableP |= enableM;
+
+	return 1;
+}
+
+int drawPixel(uint8_t x, uint8_t y, uint8_t state) {
 	return 0;
 }
 
-int draw(uint8_t x, uint8_t y, uint8_t state) {
-	return 0;
-}
-
-int love(void) {
-	return 0;
+void love(void) {
 }
 
